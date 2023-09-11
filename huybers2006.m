1ab4999fd1aebef33f41e1b2e9b93576154944eb
Bryan Lougheed <bryanlougheed@hotmail.com>
2023-09-11 22:50:17 +0200

    Add files via upload

--- huybers2006.m
+++ huybers2006.m
@@ -9,20 +9,21 @@ thresh = 275; % what huybers uses
 
 
 % get laskar et al orbital parameters
-[tka ecc obl lpe]  = getlaskar2004(1, 'slice',[-0.5 2000.5]);
+[tka ecc obl lpe]  = getlaskar2004(1, 'slice',[-0.5 300]);
+tka = round(tka);
 
 % N65 summer solstice W/m2
 [n65sswm2, ~, ~, ~] = irrwm2(lattarg, 90, con, ecc, obl, lpe);
 
 % total summer J/m2
 ndays = 365.2;
-dayres = 0.1;
+dayres = 0.2;
 sdays = 0:dayres:ndays-dayres; % day 0 and day ndays are same day
 n65Jm2thresh = NaN(size(tka));
 n65meanirrthresh = NaN(size(tka));
 secs = NaN(size(tka));
 for i = 1:numel(tka)
-	sunlons = sday2sunlon(sdays,ecc(i),lpe(i),ndays);
+	sunlons = time2sunlon(sdays,ecc(i),lpe(i),ndays);
 	[irrs, ~, ~, ~] = irrwm2(lattarg, sunlons, con, ecc(i), obl(i), lpe(i));
 	n65meanirrthresh(i) = mean(irrs(irrs>=thresh));
 	secs(i) = numel(sdays(irrs>=thresh)) * (24*60*60*dayres);
@@ -33,33 +34,35 @@ seasdays = secs / (60*60*24); % season day length (days with Wm2 greater than th
 
 % plot some stuff
 
-xlims = [0 1000];
+xlims = [0 300];
 npanels = 4;
+timedir = 'reverse';
 
 subplot(npanels,1,1)
 plot(tka,n65sswm2)
-set(gca,'xdir','reverse')
+set(gca,'xdir',timedir)
 xlim(xlims)
 ylabel('Wm^{-2}')
 xlabel('Age (ka)')
 
 subplot(npanels,1,2)
 plot(tka,n65meanirrthresh)
-set(gca,'xdir','reverse')
+set(gca,'xdir',timedir)
 xlim(xlims)
 ylabel('Wm^{-2}')
 xlabel('Age (ka)')
 
 subplot(npanels,1,3)
 plot(tka,seasdays)
-set(gca,'xdir','reverse')
+set(gca,'xdir',timedir)
 xlim(xlims)
 ylabel('Days')
 xlabel('Age (ka)')
 
 subplot(npanels,1,4)
 plot(tka,n65Jm2thresh/10^9)
-set(gca,'xdir','reverse')
+set(gca,'xdir',timedir)
 xlim(xlims)
 ylabel('GJm^{-2}')
 xlabel('Age (ka)')
+
