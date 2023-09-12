397f04bb00225905a8cf9d67deac02d550e2c172
Bryan Lougheed <bryanlougheed@hotmail.com>
2023-09-12 10:49:00 +0200

    Add files via upload

--- sinnottbasic.m
+++ sinnottbasic.m
@@ -18,6 +18,7 @@ function E = sinnottbasic(M,ecc)
 % R.W. Sinnott (1985), "A computer assault on Kepler's equation", Sky and Telescope, vol. 70, page 159.
 % Meeus, J., (1998). Chapter 30 in Astronomical Algorithms, 2nd ed. Willmann-Bell, Inc., Richmond, Virginia.
 % Kostadinov and Gilb, (2014): doi:10.5194/gmd-7-1051-2014
+
 F = sign(M);
 M = abs(M)./(2*pi);
 M = (M-floor(M))*(2*pi).*F;
@@ -27,7 +28,7 @@ F(M>pi) = -1;
 M(M>pi) = (2*pi)-M(M>pi); % inbound
 Eo = (pi/2)*ones(size(ecc));
 D = (pi/4)*ones(size(ecc));
-for j = 1:54 % Sinnot says number of iterations is 3.30 * significant figures of system. Matlab double has 16 digit precision, so 16*3.30=53. Let's use 54
+for iter = 1:54 % Sinnot says number of iterations is 3.30 * significant figures of system. Matlab double has 16 digit precision, so 16*3.30=53. Let's use 54
 	M1 = Eo-ecc.*sin(Eo);
 	Eo = Eo + D.*sign(M-M1);
 	D = D./2;
@@ -35,5 +36,4 @@ end
 E = Eo.*F;
 
 
-end
-
+end % end function
